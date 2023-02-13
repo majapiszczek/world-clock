95d599a0459847bf5b0de7ba6a671deb1382f6d5
majapiszczek <piszczekmaja33@gmail.com>
2023-02-13 10:26:14 +0100

    added current location option

--- js/index.js
+++ js/index.js
@@ -38,6 +38,10 @@ function displayTime() {
 
 function displayTimeSelected(event) {
   let cityTimeZone = event.target.value;
+
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
